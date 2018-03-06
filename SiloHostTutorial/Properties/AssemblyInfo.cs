0dc5a4ea92f64231739d9e9540bf5d0f77b04c60
Thiago Loureiro <thiagoguaru@outlook.com>
2018-03-07 00:57:59 +0100

    Update

--- SiloHostTutorial/Properties/AssemblyInfo.cs
+++ SiloHostTutorial/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SiloHostTutorial")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tutorial Microsoft Orleans")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Thiago Loureiro")]
 [assembly: AssemblyProduct("SiloHostTutorial")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
